9ab4b769eaabf10bf0d2dafcbfebaae40985c0e6
Tomasz Stolarczyk <tomasz.stolarczyk@indigo-nails.com>
2023-11-30 08:49:18 +0100

    update 2023-11-30

--- tasks/2023-11-30/index.ts
+++ tasks/2023-11-30/index.ts
@@ -1,1 +1,8 @@
-// Tutaj skopiuj kod zadania
+interface Mineral {
+    value: number;
+    mass: number;
+  }
+
+  export function optimizeLoad(minerals: Mineral[], maxMass: number): Mineral[] {
+    return [];
+  }
