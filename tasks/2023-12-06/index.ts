02e101209e943208d491b835c9c671cb786b7377
Tomasz Stolarczyk <tomasz.stolarczyk@indigo-nails.com>
2023-12-07 09:33:22 +0100

    tasks solution

--- tasks/2023-12-06/index.ts
+++ tasks/2023-12-06/index.ts
@@ -1,9 +1,43 @@
 // Tutaj skopiuj kod zadania
 
-export class OrderController {
+enum InvalidState {
+    unknown = "unknown",
+  }
 
-}
+  export class OrderController {
+    private machines = new Set<Machine>();
 
-export class Machine {
+    registerMachine(machine: Machine) {
+      this.machines.add(machine);
+    }
 
-}
+    unregisterMachine(machine: Machine) {
+      this.machines.delete(machine);
+    }
+
+    setState(state: string) {
+      if (state === InvalidState.unknown) {
+        throw new Error("Invalid state provided");
+      }
+      this.machines.forEach((machine) => {
+        machine.state = state;
+        machine.addToLog(state);
+      });
+    }
+  }
+
+  export class Machine {
+    private statesLog: string[] = [];
+
+    state: string | null = null;
+
+    performAudit() {
+      return this.statesLog.map((state, index) => {
+        return `Order #${index + 1} - ${state}`;
+      });
+    }
+
+    addToLog(state: string) {
+      this.statesLog.push(state);
+    }
+  }
