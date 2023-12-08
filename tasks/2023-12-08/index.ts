c895efd2865547606b3a865474def53a5a32973b
Tomasz Stolarczyk <tomasz.stolarczyk@indigo-nails.com>
2023-12-08 10:36:02 +0100

    add 08 task

---
+++ tasks/2023-12-08/index.ts
@@ -0,0 +1,13 @@
+// Tutaj skopiuj kod zadania
+
+export interface Letter {
+    content: string;
+    country: 'pl' | 'de' | 'us';
+    priority: 'high' | 'medium' | 'low';
+  }
+
+  export class LetterSorter {
+    sortLetters(): [] {
+      return [];
+    }
+  }
