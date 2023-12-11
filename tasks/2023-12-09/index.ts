61426171a6673ea7f3ac5e57e0d879e59636e69d
Tomasz Stolarczyk <tomasz.stolarczyk@indigo-nails.com>
2023-12-11 08:33:33 +0100

    solution on 09 taks

--- tasks/2023-12-09/index.ts
+++ tasks/2023-12-09/index.ts
@@ -1,9 +1,40 @@
 // Tutaj skopiuj kod zadania
 
 export interface Tool {
-
+	init: () => void
+	update: () => void
+	dispose: () => void
 }
 
-export class Equipment {
+const ERROR_MESSAGE_BEFORE_INIT =
+	'Cannot update any tools before initialization.'
 
+export class Equipment<T extends Tool = Tool> {
+	tools: Array<T> = []
+	initIndexes = new Set<number>()
+
+	registerTools(tool: T) {
+		this.tools.push(tool)
+	}
+
+	initializeTools() {
+		this.tools.forEach((tool, index) => {
+			tool.init()
+			this.initIndexes.add(index)
+		})
+	}
+
+	updateTools() {
+		this.tools.forEach((tool, index) => {
+			if (!this.initIndexes.has(index)) {
+				throw new Error(ERROR_MESSAGE_BEFORE_INIT)
+			}
+			tool.update()
+		})
+	}
+	disposeTools() {
+		this.tools.forEach(tool => {
+			tool.dispose()
+		})
+	}
 }
