bd657c88f50f345e1c30d4104c478a8d857a1159
Tomasz Stolarczyk <tomasz.stolarczyk@indigo-nails.com>
2023-12-14 09:01:23 +0100

    add 14 task

---
+++ tasks/2023-12-14/index.ts
@@ -0,0 +1,11 @@
+// Tutaj skopiuj kod zadania
+
+export type Gift = {
+    value: number;
+    weight: number;
+    volume: number;
+  };
+
+  export function calculateMaxGiftValue(gifts: Gift[], maxWeight: number, maxVolume: number): number {
+    return 0;
+  }
