413f547605efd4a4f3e457ab0afd25f421c11948
Tomasz Stolarczyk <tomasz.stolarczyk@indigo-nails.com>
2023-12-15 08:26:16 +0100

    add 15 task

---
+++ tasks/2023-12-15/index.ts
@@ -0,0 +1,9 @@
+// Tutaj skopiuj kod zadania
+
+export function* storageQuery(range: number, gift: string, resolver: (n: number, gift: string) => boolean): Generator<number> {
+    throw new Error('Not implemented');
+}
+
+export function storageResolver(n: number, gift: string): boolean {
+    return false;
+}
